4833054732733d7b3ccf5d8592db8e6faa90bf05
Jakub Madejczyk <jbmk01@gmail.com>
2024-09-14 00:51:34 +0200

    working version with midi sound

--- main.py
+++ main.py
@@ -1,13 +1,45 @@
 import pygame
 import random
- 
+import time
+import fluidsynth
+import threading
+
+
+# Initialize FluidSynth
+fs = fluidsynth.Synth()
+fs.start(driver="coreaudio")  # Use "coreaudio" for macOS, "dsound" for Windows, "alsa" for Linux
+sfid = fs.sfload("/Users/jakub/Downloads/FluidR3/FluidR3_GM.sf2")
+fs.program_select(0, sfid, 0, 0)
+
+pygame.init()
+
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
+DARK_GREEN = (0, 150, 0)
 RED = (255, 0, 0)
  
-pygame.init()
 
+
+# notes_sequence = [60, 62, 64, 65, 67, 69, 71, 72]
+notes_sequence = [76, 12, 76, 12, 20, 12, 76, 12, 20, 12, 72, 12, 76, 12, 20, 12, 79, 12, 20, 36, 67, 12, 20, 36, 72, 12, 20, 24, 67, 12, 20, 24, 64, 12, 20, 24, 69, 12, 20, 12, 71, 12, 20, 12, 70, 12, 69, 12, 20, 12, 67, 16, 76, 16, 79, 16, 81, 12, 20, 12, 77, 12, 79, 12, 20, 12, 76, 12, 20, 12, 72, 12, 74, 12, 71, 12, 20, 24, 48, 12, 20, 12, 79, 12, 78, 12, 77, 12, 75, 12, 60, 12, 76, 12, 53, 12, 68, 12, 69, 12, 72, 12, 60, 12, 69, 12, 72, 12, 74, 12, 48, 12, 20, 12, 79, 12, 78, 12, 77, 12, 75, 12, 55, 12, 76, 12, 20, 12, 84, 12, 20, 12, 84, 12, 84, 12, 55, 12, 20, 12, 48, 12, 20, 12, 79, 12, 78, 12, 77, 12, 75, 12, 60, 12, 76, 12, 53, 12, 68, 12, 69, 12, 72, 12, 60, 12, 69, 12, 72, 12, 74, 12, 48, 12, 20, 12, 75, 24, 20, 12, 74, 24, 20, 12, 72, 24, 20, 12, 55, 12, 55, 12, 20, 12, 48, 12
+
+, 72, 12, 72, 12, 20, 12, 72, 12, 20, 12, 72, 12, 74, 12, 20, 12, 76, 12, 72, 12, 20, 12, 69, 12, 67, 12, 20, 12, 43, 12, 20, 12, 72, 12, 72, 12, 20, 12, 72, 12, 20, 12, 72, 12, 74, 12, 76, 12, 55, 12, 20, 24, 48, 12, 20, 24, 43, 12, 20, 12, 72, 12, 72, 12, 20, 12, 72, 12, 20, 12, 72, 12, 74, 12, 20, 12, 76, 12, 72, 12, 20, 12, 69, 12, 67, 12, 20, 12, 43, 12, 20, 12, 76, 12, 76, 12, 20, 12, 76, 12, 20, 12, 72, 12, 76, 12, 20, 12, 79, 12, 20, 36, 67, 12, 20, 36
+
+, 76, 12, 72, 12, 20, 12, 67, 12, 55, 12, 20, 12, 68, 12, 20, 12, 69, 12, 77, 12, 53, 12, 77, 12, 69, 12, 60, 12, 53, 12, 20, 12, 71, 16, 81, 16, 81, 16, 81, 16, 79, 16, 77, 16, 76, 12, 72, 12, 55, 12, 69, 12, 67, 12, 60, 12, 55, 12, 20, 12, 76, 12, 72, 12, 20, 12, 67, 12, 55, 12, 20, 12, 68, 12, 20, 12, 69, 12, 77, 12, 53, 12, 77, 12, 69, 12, 60, 12, 53, 12, 20, 12, 71, 12, 77, 12, 20, 12, 77, 12, 77, 16, 76, 16, 74, 16, 72, 12, 64, 12, 55, 12, 64, 12, 60, 12, 20, 36, 72, 12, 20, 24, 67, 12, 20, 24, 64, 24, 69, 16, 71, 16, 69, 16, 68, 24, 70, 24, 68, 24, 67, 12, 65, 12, 67, 48]
+
+
+
+# Function to play a MIDI note
+def play_midi_note_thread(note, duration=0.5, velocity=127):
+    fs.noteon(0, note, velocity)
+    time.sleep(duration)
+    fs.noteoff(0, note)
+
+def play_midi_note(note, duration=0.5, velocity=127):
+    if note != 20:
+        thread = threading.Thread(target=play_midi_note_thread, args=(note, duration, velocity))
+        thread.start()
 
 def draw_grid():
     black = False
@@ -29,7 +61,8 @@ class Snake(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = pygame.Surface([20, 20])
-        self.image.fill(GREEN)
+        self.color = DARK_GREEN
+        self.image.fill(self.color)
         self.rect = self.image.get_rect()
         self.rect.x = 0
         self.rect.y = 0
@@ -37,6 +70,7 @@ class Snake(pygame.sprite.Sprite):
         self.direction = "down"
         self.next_direction = "down"
         self.length = 1
+        
 
     def move(self, x, y):
         if self.rect.x + x < 0:
@@ -57,7 +91,7 @@ class Snake(pygame.sprite.Sprite):
         pass
 
     def draw(self):
-        pygame.draw.rect(screen, GREEN, [self.rect.x, self.rect.y, 20, 20])
+        pygame.draw.rect(screen, self.color, [self.rect.x, self.rect.y, 20, 20])
 
     def get_coords(self):
         return (self.rect.x, self.rect.y)
@@ -91,6 +125,7 @@ pygame.display.set_caption("Snake, or python idk")
 done = False
 clock = pygame.time.Clock()
 snake = Snake()
+font = pygame.font.Font(None, 36)
 fruits = []
 frames_counter = 0
 snake_coords = []
@@ -106,6 +141,7 @@ for i in range(4):
     fruits_coords.append(fruits[i].get_coords())
     
 # -------- Main Program Loop -----------
+n = 0
 while not done:
     # --- Main event loop
     for event in pygame.event.get():
@@ -125,11 +161,13 @@ while not done:
                 if snake.direction != "up":
                     snake.next_direction = "down"
 
- 
+
     # --- Game logic --- #
     frames_counter += 1
+   
     # every 30 frames the snake moves
-    if frames_counter == 30:
+    
+    if frames_counter == 28:
         frames_counter = 0
 
         if snake.next_direction == "left":
@@ -147,6 +185,18 @@ while not done:
         elif snake.next_direction == "down":
             snake.direction = "down"
             snake.move(0, 20)
+        
+        if n < len(notes_sequence) - 1:
+            play_midi_note(notes_sequence[n], notes_sequence[n + 1]//12)
+            snake.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+            print(notes_sequence[n])
+
+            n += 2
+        else:
+            n = 0
+
+
+
 
         # insert the snake coords at the beginning of the snake_coords list
         snake_coords.insert(0, snake.get_coords())
@@ -163,20 +213,33 @@ while not done:
             print("Game Over")
             done = True
    
+
+    if frames_counter == 14 or frames_counter == 28:
+        if n < len(notes_sequence) - 1:
+            play_midi_note(notes_sequence[n], notes_sequence[n + 1]//12)
+            print(notes_sequence[n])
+            print(notes_sequence[n + 1]//12)
+
+            n += 2
+        else:
+            n = 0
+
     # --- Drawing code --- #
 
     draw_grid()
     snake.draw()
 
     # draw the snake tail
-    for coord in snake_coords[1:]:
-        pygame.draw.rect(screen, GREEN, [coord[0], coord[1], 20, 20])
 
+    for coord in snake_coords[1:]:
+        pygame.draw.rect(screen, GREEN, [coord[0], coord[1], 20, 20], 7)
+   
     for fr in fruits:
         # check if the snake collides with a fruit
         if fr.get_coords() == snake.get_coords():
             fruits_coords.remove(fr.get_coords())
             fruits.remove(fr)
+            play_midi_note(random.choice(notes_sequence))  # Play a random note when the snake eats a fruit
             # increase the snake length if it collides with a fruit
             snake.length += 1
             # add a new fruit
@@ -196,17 +259,21 @@ while not done:
             # draw the fruit
             fr.draw()
         
-        
+    score_text = font.render("Score: " + str(snake.length), True, (255, 255, 255))
+    screen.blit(score_text, (10, 10))
+
     # --- update the screen --- #
     pygame.display.flip()
  
     # --- Limit to 60 frames per second --- #
     clock.tick(60)
  
+
+# Close the MIDI output device
+
+
 pygame.quit()
 
-#TODO end the game when the snake collides with itself
-#TODO add a score counter
 #TODO add a score animation
 #TODO add a restart button
 #TODO add a game over screen
